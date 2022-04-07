f726fbc1c1fd9c8be43294fec287a12389d1dbc4
bikashghimire <bikashghimire2610@gmail.com>
2022-04-07 12:30:03 +0300

    nade necessary components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import NotesList from './components/NotesList'
+import {nanoid} from 'nanoid'
+import Search from './components/search';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function app() {
+  // notes example 
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is note",
+      date: "04/04/2020"
+    },
+    {
+      id: nanoid(),
+      text: "This is second  note",
+      date: "04/04/2021"
+    },
+    {
+      id: nanoid(),
+      text: "This is third note",
+      date: "04/04/2022"
+    },
+
+  ]);
+  // these are the states for searching text and toggle mode
+  const [searchNote, setSearchNote] = useState('');
+  const [darkMode, setDarkMode]     = useState(false)
+
+  // to store or save data in local storage even after closing the tab
+  useEffect(() => {
+    const savedNotes = JSON.parse(localStorage.getItem('notes-app-react-data'))
+    if(!savedNotes) {
+      setNotes(savedNotes)
+    }
+  }, [])
+  // This is for saving the app data in local storage
+  useEffect(() => {
+    localStorage.setItem('notes-app-react-data', JSON.stringify(notes))
+  }, [notes])
+
+  // This function is for adding new notes
+  const addNote = (text) =>{
+    const date = new Date();
+    const newNote = {
+      id:nanoid(),
+      text: text,
+      date: date.toLocaleDateString()
+    }
+    const newNotes = [...notes, newNote]
+    setNotes(newNotes)
+  }
+  // Following function is used to delete notes in the note app with a id
+  const deletingNote = (id) => {
+    const newNotes = notes.filter((note) => note.id!==id);
+    setNotes(newNotes)
+  }
+  return{
+    <div className = {`${darkMode && `dark-mode`}`}>
+    <div className = "container">
+    <Header handleToggleDarkMode = {setDarkMode}/>
+
+    <Search handleSearch = {setSearchNote}/>
+
+    <NotesList notes = {notes.filter((note) => note.text.toLowerCase().includes(searchNote))}
+    handleAddNote = {addNote} handleDelete = (deleteingNote)/>
+
     </div>
-  );
+    </div>
+  }
+  
 }
 
-export default App;
+export dafault App
---
+++ src/components/addNote.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function addNote() {
+  return (
+    <div>addNote</div>
+  )
+}
+
+export default addNote
